a8b1c125dd5b8738ff42a105b9410124ff638d07
Ivo_Sa92 <141323736+IvoSa92@users.noreply.github.com>
2025-10-27 18:26:11 +0100

    feat: update team section with new images and content; rename navbar title; add ngrok dependency; modify AboutHero section layout and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      host: true,
+      allowedHosts: ['cee68481756f.ngrok-free.app'],
+    },
   },
 });
